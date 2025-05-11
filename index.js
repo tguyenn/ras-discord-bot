2c3c34e77ca5df1f7ca60ac6aa57512b7cbd0e08
Toby Nguyen <tobyanguyen1@gmail.com>
2025-05-11 08:09:34 -0500

    quotes to double

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Imports
-const Discord = require('discord.js');
-const express = require('express'); // Import Express
-const bodyParser = require('body-parser'); // Import Body Parser for JSON parsing
-const config = require('./config/config.json');
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
-const pm2 = require('pm2');
+const Discord = require("discord.js");
+const express = require("express"); // Import Express
+const bodyParser = require("body-parser"); // Import Body Parser for JSON parsing
+const config = require("./config/config.json");
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+const pm2 = require("pm2");
 
 // Discord client
 var intents = [
@@ -35,11 +35,11 @@ client.slashCommands = [];
 client.commandCategories = {}; // Object of command categories
 
 // Load utils
-const getAllFiles = require('./utils/getFileLocations');
+const getAllFiles = require("./utils/getFileLocations");
 
 // Load commands
-console.log('Loading commands..');
-var commands = getAllFiles(`${__dirname}/commands/`, '.js');
+console.log("Loading commands..");
+var commands = getAllFiles(`${__dirname}/commands/`, ".js");
 for (const command of commands) {
     var cmdFile = require(command)(client, config);
     console.log(`Loading ${cmdFile.name}..`);
@@ -50,11 +50,11 @@ for (const command of commands) {
     if (cmdFile.data != null) client.slashCommands.push(cmdFile.data.toJSON());
     client.commands.set(cmdFile.name, cmdFile);
 }
-console.log(`${Array.from(client.commands.values()).length} command${Array.from(client.commands.values()).length > 1 || Array.from(client.commands.values()).length < 1 ? 's' : ''} was loaded!`);
+console.log(`${Array.from(client.commands.values()).length} command${Array.from(client.commands.values()).length > 1 || Array.from(client.commands.values()).length < 1 ? "s" : ""} was loaded!`);
 
 //Loading events
-console.log('Listening for events..');
-var events = getAllFiles(`${__dirname}/events/`, '.js');
+console.log("Listening for events..");
+var events = getAllFiles(`${__dirname}/events/`, ".js");
 for (const event of events) {
     eventFile = require(event)(client, config);
     if (eventFile.once) client.once(eventFile.name, async (...args) => {
@@ -71,9 +71,9 @@ for (const event of events) {
             if (err) console.log(err);
         }
     });
-    console.log(`Listening on '${eventFile.name}'..`);
+    console.log(`Listening on "${eventFile.name}"..`);
 }
-console.log('Listening for all events now!');
+console.log("Listening for all events now!");
 
 // Logging into the bot
 client.login(config.BOT_TOKEN);
@@ -87,41 +87,41 @@ app.use(bodyParser.json());
 
 
 // API Endpoint to update file at config/config.json
-app.post('/update-config', (req, res) => {
+app.post("/update-config", (req, res) => {
 	const config = req.body;
   
-	const configPath = path.join(__dirname, 'config', 'config.json');
+	const configPath = path.join(__dirname, "config", "config.json");
   
 	fs.writeFile(configPath, JSON.stringify(config, null, 2), (err) => {
 		if (err) {
-			console.error('Error writing config:', err);
-			return res.status(500).send('Failed to write config');
+			console.error("Error writing config:", err);
+			return res.status(500).send("Failed to write config");
 		}
 
 		pm2.connect((err) => {
 			if (err) {
-				console.error('Error connecting to PM2:', err);
-				return res.status(500).send('Failed to restart bot');
+				console.error("Error connecting to PM2:", err);
+				return res.status(500).send("Failed to restart bot");
 			}
 		
-			pm2.restart('discord-bot', (err) => {
+			pm2.restart("discord-bot", (err) => {
 				pm2.disconnect();
 				if (err) {
-					console.error('Error restarting bot:', err);
-					return res.status(500).send('Failed to restart bot');
+					console.error("Error restarting bot:", err);
+					return res.status(500).send("Failed to restart bot");
 				}
 		
-				console.log('Bot restarted successfully via PM2.');
-				res.status(200).send('Config updated and bot restarted successfully!');
+				console.log("Bot restarted successfully via PM2.");
+				res.status(200).send("Config updated and bot restarted successfully!");
 			});
 		});
-		console.log('Config updated and written to file.');
+		console.log("Config updated and written to file.");
 		res.sendStatus(200);
 	});
 });
 
 // API Endpoint to Send a Message
-app.post('/send-message', async (req, res) => {
+app.post("/send-message", async (req, res) => {
     const { content, embeds } = req.body;
 
     // Log the request body
@@ -133,7 +133,7 @@ app.post('/send-message', async (req, res) => {
 
     // Validate input
     if (!content && !embeds) {
-        return res.status(400).send({ error: 'either content or embeds are required.' });
+        return res.status(400).send({ error: "either content or embeds are required." });
     }
 
     try {
@@ -141,7 +141,7 @@ app.post('/send-message', async (req, res) => {
         let API_channelId = config.ORDERS_CH_ID;
         const API_channel = await client.channels.fetch(API_channelId);
         if (!API_channel) {
-            return res.status(404).send({ error: 'API_Channel not found.' });
+            return res.status(404).send({ error: "API_Channel not found." });
         }
         console.log(`Fetching GAS discord channel with ID: ${API_channelId}`);
 
@@ -149,25 +149,25 @@ app.post('/send-message', async (req, res) => {
         const messagePayload = {};
         if (content) messagePayload.content = content;
         if (embeds) messagePayload.embeds = embeds;
-		console.log('Copied message payload!');
+		console.log("Copied message payload!");
 		// console.log(embeds);
 
         // Create buttons (optional)
         const row = new Discord.MessageActionRow()
             .addComponents(
                 new Discord.MessageButton()
-                    .setCustomId('delete_button')
+                    .setCustomId("delete_button")
                     .setLabel(config.DISC_CONFIRM_BTN_MSG)
-                    .setStyle('DANGER')
+                    .setStyle("DANGER")
             );
         messagePayload.components = [row];
 
         // Send the message
         await API_channel.send(messagePayload);
-        return res.status(200).send({ success: true, message: 'Message sent successfully!' });
+        return res.status(200).send({ success: true, message: "Message sent successfully!" });
     } catch (error) {
-        console.error('Error sending message:', error);
-        return res.status(500).send({ error: 'Failed to send message.' });
+        console.error("Error sending message:", error);
+        return res.status(500).send({ error: "Failed to send message." });
     }
 });
 
@@ -179,14 +179,14 @@ app.listen(PORT, () => {
 
 
 // Interaction handling
-client.on('interactionCreate', async (interaction) => {
+client.on("interactionCreate", async (interaction) => {
     try {
         // Handle slash commands
         if (interaction.isCommand()) {
             const command = client.commands.get(interaction.commandName);
             if (!command) {
                 console.error(`No command matching ${interaction.commandName} was found.`);
-                await interaction.reply({ content: 'Command not found.', ephemeral: true });
+                await interaction.reply({ content: "Command not found.", ephemeral: true });
                 return;
             }
 
@@ -197,9 +197,9 @@ client.on('interactionCreate', async (interaction) => {
 
         // Handle button interactions
         if (interaction.isButton()) {
-            if (interaction.customId === 'help_previous') {
-                await interaction.reply({ content: 'You clicked the Previous button!' });
-            } else if (interaction.customId === 'delete_button') {
+            if (interaction.customId === "help_previous") {
+                await interaction.reply({ content: "You clicked the Previous button!" });
+            } else if (interaction.customId === "delete_button") {
 
                 if ((interaction.user.id === config.DISC_AMZ_ORDER_TAG) || (interaction.user.id === config.DISC_NON_AMZ_ORDER_TAG) || (interaction.user.id === config.DISC_DEBUG_TAG)) {
                     console.log(`Interaction triggered by the specific user: ${interaction.user.tag}`);
@@ -218,21 +218,21 @@ client.on('interactionCreate', async (interaction) => {
                 const processed_channel = await client.channels.fetch(processed_channel_ID);
                 console.log(`Fetching channel with ID: ${processed_channel_ID}`);
                 if (!processed_channel) {
-                    console.error('Processed channel not found.');
+                    console.error("Processed channel not found.");
                     return;
                 }
 
 				let originalContent = interaction.message.content;
-				let strippedContent = originalContent.split('\n').slice(1).join('\n'); // remove ping from message before reposting
+				let strippedContent = originalContent.split("\n").slice(1).join("\n"); // remove ping from message before reposting
                 await processed_channel.send({ 
-					content: strippedContent || 'no message content oh no',
+					content: strippedContent || "no message content oh no",
 					embeds: embeds 
 				});
 
 				// mark checkboxes in budget sheet
 				const numEmbeds = interaction.message.embeds.length;
 				const scriptURL = "https://script.google.com/macros/s/AKfycbyhxaNOacfgVzIkQDE8rUgzr6wEyYG-AzCt_DqcLRpTVY88478Y93uxJBQcHxNomPPm/exec";
-				const numItems = interaction.message.embeds[0].title.split(' ')[0]; // grab number of items to process	
+				const numItems = interaction.message.embeds[0].title.split(" ")[0]; // grab number of items to process	
                 const tag = interaction.message.embeds[numEmbeds-1].footer?.text; // grab tag from last embed
 				const committeeName = interaction.message.embeds[0].fields[0].value;
 				const data = { numItems: `${numItems}`, tag: `${tag}`, committeeName: `${committeeName}`};
@@ -242,11 +242,14 @@ client.on('interactionCreate', async (interaction) => {
                 interaction.reply({ content: `${config.DISC_CONFIRM_REPLY_MSG} ${tag}`, ephemeral: true });
                 await interaction.message.delete();
             }
+            else if(interaction.customId == "") {
+                
+            }
         }
     } catch (error) {
-        console.error('Error handling interaction:', error);
+        console.error("Error handling interaction:", error);
         if (interaction.isCommand() && !interaction.replied) {
-            await interaction.reply({ content: 'An error occurred while executing the command.', ephemeral: true });
+            await interaction.reply({ content: "An error occurred while executing the command.", ephemeral: true });
         }
     }
 });
@@ -262,14 +265,14 @@ async function readMessagesTime() {
 		try {
         // Get the current time in CDT
         const now = new Date();
-        const formatter = new Intl.DateTimeFormat('en-US', {
-            timeZone: 'America/Chicago', // CDT timezone
-            hour: 'numeric',
-            minute: 'numeric',
+        const formatter = new Intl.DateTimeFormat("en-US", {
+            timeZone: "America/Chicago", // CDT timezone
+            hour: "numeric",
+            minute: "numeric",
             hour12: false,
         });
         const [currentHour, currentMinute] = formatter.formatToParts(now)
-            .filter(part => part.type === 'hour' || part.type === 'minute')
+            .filter(part => part.type === "hour" || part.type === "minute")
             .map(part => parseInt(part.value, 10));
 
         console.log(`[TIME] It is currently ${currentHour}:${currentMinute} CDT`)
@@ -296,7 +299,7 @@ async function readMessagesTime() {
 					let numEmbeds = message.embeds.length;
                     if((numEmbeds > 0) && message.content.includes(config.DISC_AMZ_ORDER_TAG)) {
                         amazonCount++;
-                        unprocAmazonArr.push(message.embeds[numEmbeds-1].footer?.text); // push last embed's footer text (tag) into arr
+                        unprocAmazonArr.push(message.embeds[numEmbeds-1].footer?.text); // push last embed"s footer text (tag) into arr
                     }
                     if((numEmbeds > 0) && message.content.includes(config.DISC_NON_AMZ_ORDER_TAG)) {
                         nonAmazonCount++;
@@ -311,7 +314,7 @@ async function readMessagesTime() {
                 let discuss_channel_id = config.DISCUSSION_CH_ID;
                 const discuss_channel = await client.channels.fetch(discuss_channel_id);
                 if (!discuss_channel) {
-                    return res.status(404).send({ error: 'discuss_channel_id not found.' });
+                    return res.status(404).send({ error: "discuss_channel_id not found." });
                 }
                 console.log(`Fetching discussion discord channel with ID: ${discuss_channel_id}`);
                 if(amazonCount > 0) {
@@ -335,18 +338,18 @@ async function readMessagesTime() {
         }
 		
 		} catch (error) {
-			console.error('Error in readMessagesTime interval:', error);
+			console.error("Error in readMessagesTime interval:", error);
 		}
 	}, 60000); // Check every minute
 
-	process.on('SIGINT', () => { // clean up so no memory leaks
+	process.on("SIGINT", () => { // clean up so no memory leaks
 		clearInterval(intervalId);
 		process.exit(0);
 	});
 }
 
 // Call the function after the bot is ready
-client.once('ready', () => {
+client.once("ready", () => {
     console.log(`${client.user.tag} is online!`);
     readMessagesTime();
 });
