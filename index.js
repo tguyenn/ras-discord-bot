94926f9e54f2b927018c6e88121826667f296696
Toby Nguyen <tobyanguyen1@gmail.com>
2025-05-11 22:23:39 -0500

    hmmm

--- index.js
+++ index.js
@@ -308,7 +308,7 @@ client.on("interactionCreate", async (interaction) => {
                     
                 }
                 else {
-                    await interaction.editReply({ content: `someting broke😔: ${JSON.stringify(response.data, null, 2)}` || "fack it broke" });
+                    await interaction.editReply({ content: `someting broke (all emails likely not there yet. just do manually now sorry)😔: ${JSON.stringify(response.data, null, 2)}` || "fack it broke" });
                     console.error("API call failed or returned no data.");
                     console.log(JSON.stringify(response.data, null, 2));
                 }
